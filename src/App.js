e5f5b853d93513a1ea64aa34bb4d1d51af7e2d26
sallesra <salles.r@gmail.com>
2023-07-20 16:31:24 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search  from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 function App() {
 
@@ -48,6 +50,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/about'element={<About />}/>
+            <Route path='/search'element={<Search />}/>
+            <Route path='/posts/:id'element={<Post />}/>
             <Route path='/login' element={!user ? <Login />: <Navigate to="/"></Navigate>}/>
             <Route path='/register'element={!user ? <Register />: <Navigate to="/"></Navigate>}/>  
             <Route path='/posts/create' element={user ? <CreatePost />: <Navigate to="/login"></Navigate>}/> 
